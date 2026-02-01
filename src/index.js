84b3af4a957f86872c465526c52f1ab0f843c05b
maverickdeepak <imdeepakchauhan12@gmail.com>
2026-02-02 00:50:56 +0530

    add matches API with validation schemas and match status utility

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {matchesRouter} from "./routes/matches.js";
 
 const app = express();
 const port = 8000;
@@ -8,6 +9,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Welcome to Sportz API');
 });
+
+app.use('/matches', matchesRouter)
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
